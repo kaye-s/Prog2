c8e18df8e518d6a5b8918a3f0a303c6ddbad0198
Isa Bright <imbright@uwm.edu>
2025-03-26 18:03:38 -0500

    plan for bitwise operations on j-types

--- src/JTypeInstruction.java
+++ src/JTypeInstruction.java
@@ -3,6 +3,10 @@ public class JTypeInstruction implements Instruction{
 
     //Constructor(int hex)
 
+//    mnemonic = "j"
+//    opcode = (hex >> 26) OR opcode = bx000010 because it's always the same
+//    index = (hex >> 0) & bx 11 1111 1111 1111 1111 1111 1111
+
     //Getters/Setters
 
     //toString
