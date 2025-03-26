63147844e7a61f110db787a7ebce6e85fc75b852
Isa Bright <imbright@uwm.edu>
2025-03-26 18:03:20 -0500

    plan for bitwise operations on r-types

--- src/RTypeInstruction.java
+++ src/RTypeInstruction.java
@@ -3,6 +3,14 @@ public class RTypeInstruction implements Instruction{
 
     //Constructor(int hex)
 
+//    funct = (hex >> 0) & bx111111;
+//    shmt = (hex >> 6) & bx11111
+//    rd = ( hex >> 11) & bx11111;
+//    rt = ( hex >> 16) & bx11111;
+//    rs = ( hex >> 21) & bx11111;
+//    opcode = (hex >> 26);
+//    get mnemonic based on funct
+
     //Getters/Setters
 
     //toString
