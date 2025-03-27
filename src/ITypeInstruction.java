61fa4a68fcc4ec900a43b0f3ce24723be1dbbd9b
Isa Bright <imbright@uwm.edu>
2025-03-26 22:27:40 -0500

    plan for bitwise operations on i-types

--- src/ITypeInstruction.java
+++ src/ITypeInstruction.java
@@ -1,7 +1,14 @@
 public class ITypeInstruction implements Instruction{
-    //Variables mnemonic, opcode, rs, rd, immediate(offset)
+    //Variables mnemonic, opcode, rs(base), rt, immediate(offset)
 
     //Constructor(int hex)
+//    I-Type Regular
+//    imm/offset = (hex >> 0) & 0xFFFF;
+//    rt = (hex >> 16) & bx11111;
+//    rs/base = (hex >> 21) & bx11111;
+//    opcode = (hex >> 26) & bx111111;
+//    mnemonic = from opcode
+
 
     //Getters/Setters
 
