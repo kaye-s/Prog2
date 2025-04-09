49a7b3858a494755819140771cd07f5c2fe5133b
kaye-s <kayes@uwm.edu>
2025-04-09 14:17:09 -0500

    outsourced decode

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,24 @@
 public class Main {
     //GLOBAL FOR TESTING PURPOSES
     public static Instruction result;
+
     public static void main(String[] args) {
-        //Test commit m2
-        long hex = Long.parseLong(args[0], 16);
-        // Declare instruction i
+        Instruction i = decode(args[0]);
+
+    }
+    public static Instruction decode(String arg) {
+        int[] reg = new int[32];
+        long hex = Long.parseLong(arg, 16);
         Instruction i;
-        // if syscall (inst == 0000000c) --> syscall
         if(hex == 0x0000000c) {
             i = new SyscallInstruction(hex);
         } else if(hex >> 26 == 0){
-            // else if opcode = 0 --> r-type
             i = new RTypeInstruction(hex);
         } else if(hex >> 26 == 0x2) {
-            // else if opcode = 000010 --> j-type
             i = new JTypeInstruction(hex);
         } else {
-            // else --> i-type
             i = new ITypeInstruction(hex);
         }
-        // sysout(i.toString())
-        System.out.println(i);
-        result = i;
+        return(i);
     }
 }
