124690acfe6db95fcfb3e981ed8f4da3f204b4a3
kaye-s <kayes@uwm.edu>
2025-04-16 13:56:49 -0500

    various scanners added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     //GLOBAL FOR TESTING PURPOSES
@@ -14,105 +18,103 @@ public class Main {
         //In data, store the first thing, then you store the whole thing up to null, then convert string from hex to string
         // Then in map, store first thing and string
         //Find out what part of address is lui using, and use that as the label.
+        try {
+            File dataFile = new File(args[1]);
+            File textFile = new File(args[0]);
+            Scanner dataReader = new Scanner(dataFile);
+            Scanner textReader = new Scanner(textFile);
+            Scanner sc = new Scanner(System.in);
 
-        //Text needs an arraylist (for addressing)
-        ArrayList<String> instructions = new ArrayList<>();
+            //Text needs an arraylist (for addressing)
+            ArrayList<String> instructions = new ArrayList<>();
 
-        int curInst = 0;
-
-        while (curInst < instructions.size()) {
-            ++curInst;
-            Instruction i = decode(instructions.get(curInst));
-
-            // switch case for 15 mnemonics:
-            //add
-            //addiu
-            //and
-            //andi
-            //beq
-            //bne
-            //j
-            //lui
-            //lw
-            //or
-            //ori
-            //slt
-            //sub
-            //sw
-            //syscall
-            switch (i.getMnemonic()) {
-                case "add":
-                    regs[i.getRd()] = regs[i.getRs()] + regs[i.getRt()];
-                    break;
-                case "addiu":
-                    regs[i.getRt()] = regs[i.getRs()] + i.getImm();
-                    break;
-                case "and":
-                    regs[i.getRd()] = regs[i.getRs()] & regs[i.getRt()];
-                    break;
-                case "andi":
-                    regs[i.getRt()] = regs[i.getRs()] & i.getImm();
-                    break;
-                case "beq":
-                    if (regs[i.getRs()] == regs[i.getRt()]) {
-                        //branch
-                        curInst += i.getImm();
-                    }
-                    break;
-                case "bne":
-                    if (regs[i.getRs()] != regs[i.getRt()]) {
-                        //branch
-                        curInst += i.getImm();
-                    }
-                    break;
-                case "j":
-                    curInst = (i.getImm() - textStart) / 4;
-                    break;
-                case "lui":
-                    regs[i.getRt()] = i.getImm() | 0xFFFF;
-                    break;
-                case "lw":
-                    break;
-                case "or":
-                    regs[i.getRd()] = regs[i.getRs()] | regs[i.getRt()];
-                    break;
-                case "ori":
-                    regs[i.getRt()] = regs[i.getRs()] | i.getImm();
-                    break;
-                case "slt":
-                    if(regs[i.getRs()] < regs[i.getRt()])
-                        regs[i.getRd()] = 1;
-                    else {
-                        regs[i.getRd()] = 0;
-                    }
-                    break;
-                case "sub":
-                    regs[i.getRd()] = regs[i.getRs()] - regs[i.getRt()];
-                    break;
-                case "sw":
-                    break;
-                case "syscall":
-                    if(regs[2] == 1) {
-                        System.out.print(regs[4]);
-                    } else if(regs[2] == 4) {
-                        //a0 address of string, find in data list
-                    } else if(regs[2] == 5) {
-                        //Scanner and store in v0
-                    } else {
-                        System.out.print("-- program is finished running --");
-                        return;
-                    }
-                    break;
-                default:
-                    // code block
+            while (textReader.hasNextLine()) {
+                instructions.add(textReader.nextLine());
             }
+            int curInst = 0;
 
-            //Read from text and data files (2 arguments)
-            //Decode each line
-            //Do the thing
-            //(printing)
-        } // end while
-        System.out.print("-- program is finished running (dropped off bottom) --");
+            while (curInst < instructions.size()) {
+               ++curInst;
+                Instruction i = decode(instructions.get(curInst));
+
+                switch (i.getMnemonic()) {
+                    case "add":
+                        regs[i.getRd()] = regs[i.getRs()] + regs[i.getRt()];
+                        break;
+                    case "addiu":
+                        regs[i.getRt()] = regs[i.getRs()] + i.getImm();
+                        break;
+                    case "and":
+                        regs[i.getRd()] = regs[i.getRs()] & regs[i.getRt()];
+                        break;
+                    case "andi":
+                        regs[i.getRt()] = regs[i.getRs()] & i.getImm();
+                        break;
+                    case "beq":
+                        if (regs[i.getRs()] == regs[i.getRt()]) {
+                            //branch
+                            curInst += i.getImm();
+                        }
+                        break;
+                    case "bne":
+                        if (regs[i.getRs()] != regs[i.getRt()]) {
+                            //branch
+                            curInst += i.getImm();
+                        }
+                        break;
+                    case "j":
+                        curInst = (i.getImm() - textStart) / 4;
+                        break;
+                    case "lui":
+                        regs[i.getRt()] = i.getImm() | 0xFFFF;
+                        break;
+                    case "lw":
+                        break;
+                    case "or":
+                        regs[i.getRd()] = regs[i.getRs()] | regs[i.getRt()];
+                        break;
+                    case "ori":
+                        regs[i.getRt()] = regs[i.getRs()] | i.getImm();
+                        break;
+                    case "slt":
+                        if(regs[i.getRs()] < regs[i.getRt()])
+                            regs[i.getRd()] = 1;
+                        else {
+                            regs[i.getRd()] = 0;
+                        }
+                        break;
+                    case "sub":
+                        regs[i.getRd()] = regs[i.getRs()] - regs[i.getRt()];
+                        break;
+                    case "sw":
+                        break;
+                    case "syscall":
+                        if(regs[2] == 1) {
+                            System.out.print(regs[4]);
+                        } else if(regs[2] == 4) {
+                            //a0 address of string, find in data list
+                        } else if(regs[2] == 5) {
+                            //Scanner and store in v0
+                            regs[2] = sc.nextInt();
+                        } else {
+                            System.out.print("\n-- program is finished running --");
+                            return;
+                        }
+                        break;
+                    default:
+                    // code block
+                }
+
+            } // end while
+            dataReader.close();
+            textReader.close();
+            sc.close();
+        } catch (Exception e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        System.out.print("\n-- program is finished running (dropped off bottom) --");
+
     }
     public static Instruction decode(String arg) {
         int[] reg = new int[32];
