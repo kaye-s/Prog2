c8c11fa7bf70dede73a905ed7d42aab9d0023673
Isa Bright <imbright@uwm.edu>
2025-04-23 14:31:53 -0500

    test for lw and changes to long

--- Test/TestLW.java
+++ Test/TestLW.java
@@ -15,7 +15,7 @@ public class TestLW {
        mem[1] = 0x12;
        mem[2] = 0xcd;
        mem[3] = 0x34;
-       Assert.assertEquals(Main.lwDecode(mem, i), 0xab12cd34);
+        Assert.assertEquals(Main.lwDecode(mem, i), 0xab12cd34);
 
     }
 }
